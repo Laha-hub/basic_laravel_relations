401d325ac40f40ed5eb95eddce1e34ccc0d33a10
Laha-hub <81339797 +Laha-hub@users.noreply.github.com>
2021-12-19 11:41:18 +0900

    #6-7 外部キー制約の動作確認、hasMany()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -13,4 +13,15 @@ class Post extends Model
         'title',
         'body',
     ];
+
+    // view に$postというデータを渡した場合、$post->commentsで関連するコメントを取得できれば便利
+    // そのためには、同名のメソッドを定義し、Commentモデルとのリレーションを設定する必要がある
+    public function comments ()
+    {
+        // モデル同士の関係を指定する
+        // 今回のケースでは、特定のポストに対して、コメントは複数あるので、hasMany()を使う
+        // return $this->hasMany('App\Models\Comment');
+        // return $this->hasMany(App\Models\Comment::class);
+        return $this->hasMany(Comment::class);
+    }
 }
